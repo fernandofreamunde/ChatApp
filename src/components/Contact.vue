e5dd70f0250489265d36095cfe45f0145f9d7a90
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-21 08:53:29 +0200

    :sparkles: contacts + contact finder components

---
+++ src/components/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <li class="sidebar-item">
+    <div class="contact-list-item">{{ contact.email }}</div>
+  </li>
+</template>
+
+<script>
+
+export default {
+  props: {
+    contact:{
+        type:Object,
+        required:true
+    },
+  },
+  data () {
+    return {
+      state: {
+        errors: [],
+      },
+    }
+  },
+  methods:{
+  },
+  created() {
+    console.log(this.contact);
+  }
+}
+</script>
+
+<style scoped>
+.contact-list-item {
+  color: #cecece;
+  margin-left:1.5em;
+}
+</style>
---
+++ src/components/ContactFinder.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="contact-finder">
+        <input class="contact-query" v-on:keyup.enter="search" type="text" v-model="searchEmail" placeholder="contact email">
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      searchEmail:''
+    }
+  },
+  methods:{
+    search:function(){
+      //query be if email exist and is not a contact yet
+      //if it is then emit event and clear textbox
+      this.$emit('searchEmail', this.searchEmail);
+      this.searchEmail = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+.contact-finder {
+  margin-left: 1.5em;
+}
+.contact-query {
+  background:none; 
+  border:none;
+  color:#ced4da;
+}
+</style>
