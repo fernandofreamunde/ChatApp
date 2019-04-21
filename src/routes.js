c7113e199834e3f31c8f7e4e6fd5790e8c63288f
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-21 08:50:34 +0200

    :sparkles: add app routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import login from './components/Login.vue';
 import register from './components/Register.vue';
+import notImplemented from './components/NotImplemented.vue';
+import chat from './components/Chat.vue';
 
 export default [
     {path:'/login', component:login},
-    {path:'/register', component:register}
+    {path:'/register', component:register},
+    {path:'/woops', component:notImplemented},
+    {path:'/chat', component:chat},
 ] 
