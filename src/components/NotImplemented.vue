d6803ebe86eb9ae49eb5c8a3b7403462a155141f
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-21 08:51:18 +0200

    :sparkles: new feature not implemented apology component

---
+++ src/components/NotImplemented.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="sorry">
+    
+    <h1 class="h3 mb-3 font-weight-normal">Sorry!</h1>
+    <p>Sorry this is part of an accessment therefore may not have all the features shown in the UI.</p>
+    <p>I know this is bad UX, if I was going to actually deploy this I would not display any link or button to unimplemented features.</p>
+    <p>Thank you for understanding,
+      <br>
+      Fernando
+    </p>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      state: {
+        errors: [],
+      },
+      email:'',
+      password:'',
+    }
+  },
+  methods:{
+    login: function(){
+
+      console.log(this.email);
+      console.log(this.password);
+    }
+  }
+}
+</script>
+
+<style scoped>
+body {
+    background-color: #fff;
+}
+.sorry {
+    width: 100%;
+    max-width: 45em;
+    padding: 15px;
+    margin: auto;
+    margin-top: 50px;
+}
+</style>
