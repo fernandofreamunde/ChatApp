33b6cff1957f5f4ce241cf3040ed8c394c660c4e
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-21 08:52:42 +0200

    :sparkles: message component

---
+++ src/components/Message.vue
@@ -0,0 +1,42 @@
+<template>
+      <div v-bind:class="{ author: this.state.isAuthor }" class="card">
+          <div class="card-body">
+            {{ message.content }}
+          </div>
+      </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    message:{
+        type:Object,
+        required:true
+    },
+  },
+  data () {
+    return {
+      state: {
+        errors: [],
+      },
+    }
+  },
+  methods:{
+  },
+  created() {
+    this.state.isAuthor = this.message.author == "me";
+    console.log(this.message);
+    //content
+    //author
+    //timestamp
+  }
+}
+</script>
+
+<style scoped>
+
+.author {
+  text-align: right;
+}
+</style>
