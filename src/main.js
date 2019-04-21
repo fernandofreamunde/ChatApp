68471d72691c9aefdb19bc8c38ed8cebcadaef06
Fernando <fernandofreamunde@gmail.com>
2019-04-21 23:22:26 +0200

    :heavy_plus_sign: add vue resource package

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
 import Routes from './routes'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const router = new VueRouter({
   routes: Routes,
