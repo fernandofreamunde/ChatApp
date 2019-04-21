14e5f7c3cdeb175ee534185a2344f851b311c0b2
Fernando <fernandofreamunde@gmail.com>
2019-04-21 02:38:12 +0200

    :sparkles: add vue routing system

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import login from './components/Login.vue';
+import register from './components/Register.vue';
+
+export default [
+    {path:'/login', component:login},
+    {path:'/register', component:register}
+] 
