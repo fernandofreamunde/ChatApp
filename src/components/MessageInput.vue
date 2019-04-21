e1cc9500caec2bb3ed75063b508efda5a68fa962
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-21 08:52:31 +0200

    :sparkles: new message input conponent

---
+++ src/components/MessageInput.vue
@@ -0,0 +1,45 @@
+<template>
+      <div class="card">
+          <div class="card-body">
+            <input type="text" v-on:keyup.enter="send" v-model="message">
+          </div>
+      </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      state: {
+        errors: [],
+      },
+      message:''
+    }
+  },
+  methods:{
+    send() {
+      if (this.message.length === 0){
+        console.log('nothing to be done!');
+        return;
+      }
+      console.log(this.message);
+      console.log('post message to BE');
+      //emit evend new message sent
+      this.message='';
+    }
+  },
+  created() {
+    //this.state.isAuthor = this.message.author == "me";
+    //console.log(this.message);
+
+  }
+}
+</script>
+
+<style scoped>
+
+input {
+  width: 100%;
+}
+</style>
