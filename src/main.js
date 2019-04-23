e2ce181caeeedf79792f3da708c01836046414d3
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-23 05:05:47 +0200

    :heavy_plus_sign: add jwtDecoder

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import Routes from './routes'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import VueJwtDecode from 'vue-jwt-decode'
+
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
+Vue.use(VueJwtDecode);
 
 const router = new VueRouter({
   routes: Routes,
