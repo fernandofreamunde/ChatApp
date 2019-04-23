1db186b9fa05729ac9153a10a5517d70b7426ab3
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-23 05:07:40 +0200

    :sparkles: use JWT

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-    <router-view v-on:TokenRecieved="setUserToken($event)" v-bind:token="this.userTonken"></router-view>
+    <router-view 
+    v-on:TokenRecieved="setUserToken($event)" 
+    v-bind:token="this.userTonken" 
+    v-bind:username="this.username" 
+    v-bind:userAvatar="this.userAvatar"
+    ></router-view>
   </div>
 </template>
 
 <script>
+import VueJwtDecode from 'vue-jwt-decode'
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       userTonken: '',
+      username: '',
     }
   },
   methods: {
@@ -19,13 +27,10 @@ export default {
     },
     setUserToken($event){
       this.userTonken = $event;
-      console.log(this.userTonken);
+      const token = VueJwtDecode.decode(this.userTonken);
+      this.username = token.username;
+      this.userAvatar = 'https://robohash.org/'+this.username+'/?size=100x100';
       this.$router.push('/chat');
-    }
-  },
-  created() {
-    if (this.userTonken.length == 0) {
-      this.$router.push('/login');
     }
   }
 }
